dad0e4d0d0926959b36be36c45180c6abb930ddd
skpntsv <n1skopintsev@gmail.com>
2024-07-03 17:47:55 +0700

    init project

---
+++ Course-Handicap-calculation/src/main/java/ru/golfpad/skopintsev/coursehandicap/CourseHandicapCalculationApplication.java
@@ -0,0 +1,13 @@
+package ru.golfpad.skopintsev.coursehandicap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseHandicapCalculationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseHandicapCalculationApplication.class, args);
+	}
+
+}
---
+++ Course-Handicap-calculation/src/test/java/ru/golfpad/skopintsev/coursehandicap/CourseHandicapCalculationApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.golfpad.skopintsev.coursehandicap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseHandicapCalculationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
